8ed985fc6b34ded903dbb03e34aa22c77acd6af9
abc <abc@DESKTOP-ID1FAA7>
2016-07-27 17:23:32 +0530

    Add project files.

---
+++ ZMWA/Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ZMWA.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        public int Mobile { get; set; }
+        public string Message { get; set; }
+
+    }
+
+    public class ContactDBContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ ZMWA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZMWA.Startup))]
+namespace ZMWA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
